c8bacd529e9b1cca381620e07a168730edafdbda
su-mahesh <maheshkangude@gmail.com>
2021-01-14 20:21:54 +0530

    ADDED: change game status

--- TicTacToe.java
+++ TicTacToe.java
@@ -5,6 +5,7 @@ class TicTacToe
 	final int boardSize = 10;
 	char computerLetter = ' ';
 	char playerLetter = ' ';
+	char currentPlayer = ' ';
 	char toss = ' ';
 	char board[] = new char[boardSize];
 	Scanner sc = new Scanner(System.in);
@@ -13,6 +14,10 @@ class TicTacToe
 		for(int i = 0; i < boardSize; i++)
 			board[i] = ' ';
 			System.out.println("**Game started**");	
+	}
+	
+	public char getCurrentPlayer(){
+	return currentPlayer;
 	}
 	
 	public void inputSymbol(){
@@ -29,7 +34,7 @@ class TicTacToe
 		computerLetter = 'O';
 	case 'o':
 	case 'O':
-		playerLetter = 'X';
+		playerLetter = 'O';
 		computerLetter = 'X';
 	}
 	}
@@ -38,7 +43,7 @@ class TicTacToe
 	}	
 	
 	public void showBoard(){
-	int charIndex = 0;
+	int charIndex = 1;
 	for(int i = 1; i <= 3; i++){
 	
 		for(int j = 1; j <= 3; j++){
@@ -57,6 +62,8 @@ class TicTacToe
 	}
 	
 	public void drawMove(){
+	currentPlayer = playerLetter;
+	
 		System.out.println("draw move, enter index: ");
 		int index = sc.nextInt();	
 		if(index > 0 && index < boardSize)
@@ -71,8 +78,10 @@ class TicTacToe
 		else
 		System.out.println("wrong index");
 	}
-	public void computerMove()	{
-		System.out.println("computer move");
+	
+	public void computersMove()	{
+		currentPlayer = computerLetter;
+		System.out.println("computer's move");
 	}
 	public void toss(){
 	
@@ -81,16 +90,74 @@ class TicTacToe
 	String tossResult = (int) (Math.random() * 10) % 2	== 0 ? "Heads" : "Tails";
 	System.out.println("Tossed: "+tossResult);
 	
-	if( toss != tossResult.charAt(0))
-		computerMove();		
+	if( toss != tossResult.charAt(0)){
+		computersMove();
+		currentPlayer = computerLetter;		
 	}
+	else currentPlayer = playerLetter;
+	}
+	
+	public boolean checkBoardStatus(){
+	boolean status = false;
+		for (char ch : board){
+			if(ch == ' '){
+			status = true;
+			}			
+		}	
+		if(status == false){
+		System.out.println("No place left, game tie");
+		return false;
+		}
+
+		return true;
+	}
+
+	public boolean checkGameStatus(){
+		
+		if(currentPlayer == board[1] && currentPlayer == board[2] && currentPlayer == board[3] ||
+			currentPlayer == board[4] && currentPlayer == board[5] && currentPlayer == board[6] ||
+			currentPlayer == board[7] && currentPlayer == board[8] && currentPlayer == board[9] ||
+			
+			currentPlayer == board[1] && currentPlayer == board[4] && currentPlayer == board[7] ||
+			currentPlayer == board[2] && currentPlayer == board[5] && currentPlayer == board[8] ||
+			currentPlayer == board[3] && currentPlayer == board[6] && currentPlayer == board[9] ||
+			
+			currentPlayer == board[1] && currentPlayer == board[5] && currentPlayer == board[9] ||
+			currentPlayer == board[7] && currentPlayer == board[5] && currentPlayer == board[3] ){
+				
+				if(currentPlayer == playerLetter){
+					System.out.println("You Won");
+				}
+				else
+					System.out.println("Computer Won");		
+				return false;
+				}
+			else
+				return true;				
+	}	
+	
 	public static void main(String... args){
 	
 		TicTacToe TicTacToeGame = new TicTacToe();		
 		TicTacToeGame.inputSymbol();		
 		TicTacToeGame.showBoard();
 		TicTacToeGame.toss();
-		TicTacToeGame.drawMove();
-
-	}
+		int c = 2;
+		while(TicTacToeGame.checkGameStatus() && TicTacToeGame.checkBoardStatus()){
+				
+				switch(c % 2){
+					case 0:
+					TicTacToeGame.drawMove();	
+					TicTacToeGame.showBoard();
+					break;
+					case 1:
+					TicTacToeGame.computersMove();	
+					TicTacToeGame.showBoard();
+					break;
+		}
+			c++;
+		}
+		
+		}
+	
 }
