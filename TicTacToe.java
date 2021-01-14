f9ed27f5e618b670b7a412ab97ae586ad561656b
su-mahesh <maheshkangude@gmail.com>
2021-01-14 16:40:43 +0530

    ADDED: show game board

--- TicTacToe.java
+++ TicTacToe.java
@@ -6,15 +6,59 @@ class TicTacToe
 	char computerLetter = ' ';
 	char palyerLetter = ' ';
 	char board[] = new char[boardSize];
+	Scanner sc = new Scanner(System.in);
 	
 	TicTacToe(){		
 		for(int i = 0; i < boardSize; i++)
 			board[i] = ' ';
 			System.out.println("**Game started**");	
 	}
+	
+	public void inputSymbol(){
+	System.out.println("Enter your symbol X or O :");
+	
+	char input = sc.next().charAt(0);
+		if(input == 'X' || input == 'O' || input == 'x' || input == 'o')
+	{
+	switch(input)
+	{
+	case 'x':
+	case 'X': 
+		palyerLetter = 'X';
+		computerLetter = 'O';
+	case 'o':
+	case 'O':
+		palyerLetter = 'X';
+		computerLetter = 'X';
+	}
+	}
+	else
+		System.out.println("wrong input");	
+	}	
+	
+	public void showBoard(){
+	int charIndex = 0;
+	for(int i = 1; i <= 3; i++){
+	
+		for(int j = 1; j <= 3; j++){
+			System.out.print(" "+board[charIndex]+" ");
+			charIndex++;
+			if(j !=  3)
+			System.out.print("|");
+		}
+		System.out.println("");
 		
+		for(int j = 1; j <= 11 && i != 3; j++)
+			System.out.print("-");
+			System.out.println("");				
+	}
+	System.out.println("");
+	}
 	public static void main(String... args){
 	
 		TicTacToe TicTacToeGame = new TicTacToe();			
+		TicTacToeGame.inputSymbol();
+		TicTacToeGame.showBoard();
+
 	}
 }
